a786099c28845ab96bb8cb5b553deee82ca596d1
silverman42 <snkeze9@gmail.com>
2021-03-16 07:48:03 +0100

    Update: Setting helper return array of settings by default

--- app/Helpers/Settings.php
+++ app/Helpers/Settings.php
@@ -4,13 +4,33 @@ use App\Models\Settings;
 if(!function_exists('settings')){
     function settings(bool $shouldReturnDefault = false):array{
         $app_url = config('app.url');
-        if(config('app.env') == 'production' || 'development'){
-            $settings = Settings::first();
-        }
-        else{
-            $settings = null;
-        }
-        $default = [
+        // if(config('app.env') == 'production' || 'development'){
+        //     $settings = Settings::first();
+        // }
+        // else{
+        //     $settings = null;
+        // }
+        // $default = [
+        //     'app_name' => 'MMBP',
+        //     'app_email' => 'admin@mmmbp.com',
+        //     'app_url' => $app_url,
+        //     'color_scheme' => 'gray',
+        //     'naira_charge' => 0.00,
+        //     'dollar_charge' => 0.00,
+        //     'disable_client_profile_modification' => false,
+        //     'disable_client_registration' => false,
+        //     'recieve_client_emails' => false,
+        //     'auth_logo' => "{$app_url}/img/app_logo.svg",
+        //     'auth_logo_id' => null,
+        //     'dashboard_logo' => "{$app_url}/img/dashboard_logo.svg",
+        //     'dashboard_logo_id' => null
+        // ];
+        // if(!$settings || $shouldReturnDefault){
+        //     return $default;
+        // }
+        // return $settings->toArray();
+
+        return [
             'app_name' => 'MMBP',
             'app_email' => 'admin@mmmbp.com',
             'app_url' => $app_url,
@@ -25,9 +45,5 @@ if(!function_exists('settings')){
             'dashboard_logo' => "{$app_url}/img/dashboard_logo.svg",
             'dashboard_logo_id' => null
         ];
-        if(!$settings || $shouldReturnDefault){
-            return $default;
-        }
-        return $settings->toArray();
     }
 }
